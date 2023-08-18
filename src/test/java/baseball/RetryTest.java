92c1ad0b0a9573140b5ae9f5a89b1e056e1ea271
yuseonjun <seonjun0906@gmail.com>
2023-08-19 00:41:06 +0900

    test: 재시작 입력이 한자리가 아닐때 테스트 추가

--- src/test/java/baseball/RetryTest.java
+++ src/test/java/baseball/RetryTest.java
@@ -33,4 +33,16 @@ class RetryTest {
 			.isInstanceOf(IllegalArgumentException.class)
 			.hasMessage("입력된 값은 숫자가 아닙니다.");
 	}
+
+	@Test
+	void retry_입력_사이즈_테스트() {
+		//given <= 이렇게 주어졌을때
+
+		//when <= 이걸 실행하면
+
+		//then <= 이렇게 나와야해
+		assertThatThrownBy(() -> new Retry("12"))
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessage("입력은 한글자만 가능합니다.");
+	}
 }
