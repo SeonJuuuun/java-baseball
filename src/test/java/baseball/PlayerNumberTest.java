ced2541b4b7e418c0e947560da175e81bb81965a
yuseonjun <seonjun0906@gmail.com>
2023-08-19 00:35:14 +0900

    test: 입력이 오직 1~9 사이의 숫자만 가능한지 확인하는 테스트 추가

--- src/test/java/baseball/PlayerNumberTest.java
+++ src/test/java/baseball/PlayerNumberTest.java
@@ -26,6 +26,14 @@ class PlayerNumberTest extends NsTest{
 		);
 	}
 
+	@Test
+	void 플레이어_입력_범위_테스트() {
+		assertSimpleTest(() ->
+			assertThatThrownBy(() -> runException("012"))
+				.isInstanceOf(IllegalArgumentException.class)
+		);
+	}
+
 	@Override
 	public void runMain() {
 		Application.main(new String[]{});
