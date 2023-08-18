b1b03c74af16dd448116f769efd902a0e34780d7
yuseonjun <seonjun0906@gmail.com>
2023-08-19 00:22:31 +0900

    fix: 사용자 입력 오직 숫자(1~9)만 가능한 예외처리 구현

--- src/main/java/baseball/domain/PlayerNumber.java
+++ src/main/java/baseball/domain/PlayerNumber.java
@@ -21,6 +21,15 @@ public class PlayerNumber {
 		}
 	}
 
+	private void validateInputNumbers(List<Integer> playerNumbers) throws IllegalArgumentException {
+		for (Integer num : playerNumbers) {
+			String numStr = num.toString();
+			if (!numStr.matches("^[1-9]$")) {
+				throw new IllegalArgumentException("입력된 숫자는 1이상 9이하의 숫자만 가능합니다.");
+			}
+		}
+	}
+
 	public List<Integer> getPlayerNumber() {
 		return playerNumber;
 	}
