ce566bfc880b72b0f7b47949fe650e88ac50f54d
yuseonjun <seonjun0906@gmail.com>
2023-08-19 00:19:42 +0900

    feat: 사용자 도메인 구현

---
+++ src/main/java/baseball/domain/PlayerNumber.java
@@ -0,0 +1,12 @@
+package baseball.domain;
+
+import java.util.List;
+
+public class PlayerNumber {
+
+	private final List<Integer> playerNumber;
+
+	public PlayerNumber(List<Integer> playerNumber) {
+		this.playerNumber = playerNumber;
+	}
+}
