4b69bb8d6f4c5945601a58a48c9163c14a3c9215
yuseonjun <seonjun0906@gmail.com>
2023-08-19 00:21:25 +0900

    fix: 사용자 입력 중복 예외처리 구현

--- src/main/java/baseball/domain/PlayerNumber.java
+++ src/main/java/baseball/domain/PlayerNumber.java
@@ -1,6 +1,8 @@
 package baseball.domain;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class PlayerNumber {
 
@@ -8,5 +10,18 @@ public class PlayerNumber {
 
 	public PlayerNumber(List<Integer> playerNumber) {
 		this.playerNumber = playerNumber;
+	}
+
+	private void duplicatePlayerNumber(List<Integer> playerNumber) throws IllegalArgumentException {
+		Set<Integer> numberSet = new HashSet<>();
+		for (int num : playerNumber) {
+			if (!numberSet.add(num)) {
+				throw new IllegalArgumentException("입력된 숫자는 중복될 수 없습니다.");
+			}
+		}
+	}
+
+	public List<Integer> getPlayerNumber() {
+		return playerNumber;
 	}
 }
