3cccd7febfe2473b983927e779e803cb5432304b
yuseonjun <seonjun0906@gmail.com>
2023-08-19 00:25:54 +0900

    feat: 사용자 숫자 입력문구 출력

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -40,4 +40,8 @@ public class InputView {
 	public void gameStartMessage(){
 		System.out.println("숫자 야구 게임을 시작합니다.");
 	}
+
+	public void printInputMessage(){
+		System.out.print("숫자를 입력해 주세요 : ");
+	}
 }
