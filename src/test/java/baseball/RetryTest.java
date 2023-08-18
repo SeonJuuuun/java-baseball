3d487189a9fd40737b2e0ac1350c5e948baff14b
yuseonjun <seonjun0906@gmail.com>
2023-08-19 00:40:41 +0900

    test: 재시작 입력이 숫자가 아닐때 추가

--- src/test/java/baseball/RetryTest.java
+++ src/test/java/baseball/RetryTest.java
@@ -21,4 +21,16 @@ class RetryTest {
 			.isInstanceOf(IllegalArgumentException.class)
 			.hasMessage("입력된 숫자는 1 또는 2 이어야 합니다.");
 	}
+
+	@Test
+	void retry_영어_입력_테스트() {
+	    //given <= 이렇게 주어졌을때
+
+	    //when <= 이걸 실행하면
+
+	    //then <= 이렇게 나와야해
+		assertThatThrownBy(() -> new Retry("e"))
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessage("입력된 값은 숫자가 아닙니다.");
+	}
 }
