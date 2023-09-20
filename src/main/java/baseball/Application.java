2a353488d137ba8a1a53767ca7d005eb2be322cc
yuseonjun <seonjun0906@gmail.com>
2023-09-20 16:29:55 +0900

    기존 했던 코드가 남아있어 초기화

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,11 +1,8 @@
 package baseball;
 
-import baseball.controller.Game;
 
 public class Application {
+    public static void main(String[] args) {
 
-	public static void main(String[] args) {
-		Game game = new Game();
-		game.play();
-	}
+    }
 }
