accfdb1b2ad71405f5139fccb7ad7617d7b0bb2f
yuseonjun <seonjun0906@gmail.com>
2023-09-20 16:29:35 +0900

    기존 했던 코드가 남아있어 초기화

--- src/main/java/baseball/constants/Constants.java
+++
@@ -1,9 +0,0 @@
-package baseball.constants;
-
-public class Constants {
-
-	public static final int MAX_SIZE = 3;
-	public static final int successStrikeNum = 3;
-	public static final int MIN_VALUE = 1;
-	public static final int MAX_VALUE = 9;
-}
